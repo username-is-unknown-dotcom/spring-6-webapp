ddc5d6e5c7813705aa04e367fdee5bf1b4028da8
fanxychild <fbringer99@gmail.com>
2024-10-12 13:02:25 +0800

    Update

--- src/main/java/guru/springframework/spring6webapp/Spring6WebappApplication.java
+++ src/main/java/guru/springframework/spring6webapp/Spring6WebappApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Spring6WebappApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(Spring6WebappApplication.class, args);
     }
-
 }
